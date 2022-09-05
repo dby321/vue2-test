066ae11f36832ea87af589b50013ec6fa8a7174e
dby <1017834057@qq.com>
2022-09-05 22:00:20 +0800

    - add nprogress

--- src/api/request.js
+++ src/api/request.js
@@ -1,15 +1,20 @@
 // 对axios二次封装
 import axios from 'axios';
+// 引入进度条
+import nprogress from 'nprogress';
+import 'nprogress/nprogress.css'
 const requests=axios.create({
    baseURL:'/api',
    timeout:5000,
 });
 // 请求拦截器
 requests.interceptors.request.use((config)=>{
+    nprogress.start();
     return config;
 });
 // 响应拦截器
 requests.interceptors.response.use((res)=>{
+    nprogress.done();
     return res;
 },(err)=>{
     return Promise.reject(err);
